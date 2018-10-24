088af6ce668ee3df13eadf263a6e687771496d65
jonathanmusila <jonathanmusila6@gmail.com>
2018-10-25 00:09:05 +0300

    [Finishes Chore #161464392] Removed unwanted imports

--- Application/app/api/v1/views/products.py
+++ Application/app/api/v1/views/products.py
@@ -3,7 +3,6 @@
     Authored by: Jonathan Musila
 """
 from flask_restplus import Resource
-from flask_jwt_extended import jwt_required
 from app.api.v1.models.products import Products, api, product, product_update
 """
 Product List Class
